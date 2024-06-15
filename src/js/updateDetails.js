cb033be8e9ede4c702a1d46c73cfbc184fb5c55a
Alexander <atlexeide@gmail.com>
2024-06-16 00:44:38 +0200

    Refactor wind direction check

--- src/js/updateDetails.js
+++ src/js/updateDetails.js
@@ -23,24 +23,23 @@ export async function printData(dataObj) {
     const compassWindDirText = document.getElementById('compass-wind-dir-text');
     compassWindDirText.textContent = data.wind_dir;
     const windDirIcon = document.getElementById('wind-dir-icon');
-    data.wind_dir === 'N' ? windDirIcon.style.transform = 'rotate(0deg)' :
-        data.wind_dir === 'NNE' ? windDirIcon.style.transform = 'rotate(22.5deg)' :
-            data.wind_dir === 'NE' ? windDirIcon.style.transform = 'rotate(45deg)' :
-                data.wind_dir === 'ENE' ? windDirIcon.style.transform = 'rotate(67.5deg)' :
-                    data.wind_dir === 'E' ? windDirIcon.style.transform = 'rotate(90deg)' :
-                        data.wind_dir === 'ESE' ? windDirIcon.style.transform = 'rotate(112.5deg)' :
-                            data.wind_dir === 'SE' ? windDirIcon.style.transform = 'rotate(135deg)' :
-                                data.wind_dir === 'SSE' ? windDirIcon.style.transform = 'rotate(157.5deg)' :
-                                    data.wind_dir === 'S' ? windDirIcon.style.transform = 'rotate(180deg)' :
-                                        data.wind_dir === 'SSW' ? windDirIcon.style.transform = 'rotate(202.5deg)' :
-                                            data.wind_dir === 'SSW' ? windDirIcon.style.transform = 'rotate(202.5deg)' :
-                                                data.wind_dir === 'SW' ? windDirIcon.style.transform = 'rotate(225.5deg)' :
-                                                    data.wind_dir === 'WSW' ? windDirIcon.style.transform = 'rotate(247.5deg)' :
-                                                        data.wind_dir === 'W' ? windDirIcon.style.transform = 'rotate(270deg)' :
-                                                            data.wind_dir === 'WNW' ? windDirIcon.style.transform = 'rotate(292.5deg)' :
-                                                                data.wind_dir === 'NW' ? windDirIcon.style.transform = 'rotate(315deg)' :
-                                                                    data.wind_dir === 'NNW' ? windDirIcon.style.transform = 'rotate(337.5deg)' :
-                                                                        console.log('Oopsie')
+    if (data.wind_dir === 'N') { windDirIcon.style.transform = 'rotate(0deg)' };
+    if (data.wind_dir === 'NNE') { windDirIcon.style.transform = 'rotate(22.5deg)' };
+    if (data.wind_dir === 'NE') { windDirIcon.style.transform = 'rotate(45deg)' };
+    if (data.wind_dir === 'ENE') { windDirIcon.style.transform = 'rotate(67.5deg)' };
+    if (data.wind_dir === 'E') { windDirIcon.style.transform = 'rotate(90deg)' };
+    if (data.wind_dir === 'ESE') { windDirIcon.style.transform = 'rotate(112.5deg)' };
+    if (data.wind_dir === 'SE') { windDirIcon.style.transform = 'rotate(135deg)' };
+    if (data.wind_dir === 'SSE') { windDirIcon.style.transform = 'rotate(157.5deg)' };
+    if (data.wind_dir === 'S') { windDirIcon.style.transform = 'rotate(180deg)' };
+    if (data.wind_dir === 'SSW') { windDirIcon.style.transform = 'rotate(202.5deg)' };
+    if (data.wind_dir === 'SSW') { windDirIcon.style.transform = 'rotate(202.5deg)' };
+    if (data.wind_dir === 'SW') { windDirIcon.style.transform = 'rotate(225.5deg)' };
+    if (data.wind_dir === 'WSW') { windDirIcon.style.transform = 'rotate(247.5deg)' };
+    if (data.wind_dir === 'W') { windDirIcon.style.transform = 'rotate(270deg)' };
+    if (data.wind_dir === 'WNW') { windDirIcon.style.transform = 'rotate(292.5deg)' };
+    if (data.wind_dir === 'NW') { windDirIcon.style.transform = 'rotate(315deg)' };
+    if (data.wind_dir === 'NNW') { windDirIcon.style.transform = 'rotate(337.5deg)' };
 };
 
 export async function changeUnits(dataObj) {
